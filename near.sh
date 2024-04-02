a642e6e5c630cbd1039224a56a3a1883b8fca779
jumplik <165834202+jumplik@users.noreply.github.com>
2024-04-03 01:49:43 +0700

    Initial commit

---
+++ near.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://near.lava.build/lava-referer-e9fd0045-01cd-4d07-a874-53d2cef58ec1/"
+
+OUTPUT_DIR="logs"
+
+near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$near_data" >> "$OUTPUT_DIR/near.txt"
+    
+    echo "NEAR data fetched and saved to $OUTPUT_DIR/near.txt."
+else
+    echo "Failed to fetch NEAR data."
+fi
